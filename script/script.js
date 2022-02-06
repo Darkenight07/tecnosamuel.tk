8528660a5f7275c5b7975a9c9491e88ae01017fa
OMGSAMUELRBR <66468335+OMGSAMUELRBR@users.noreply.github.com>
2022-02-06 01:05:37 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-
+var mensaje = confirm("Esta web esta en desarrollo, ¿te gustaría entrar a mi web?");
+//Detectamos si el usuario acepto el mensaje
+if (mensaje) {
+    alert("Gracias por aceptar, te dirigeremos a mi web");
+}
+//Detectamos si el usuario denegó el mensaje
+else {
+alert("Te dirigeremos a Google");
+    window.location.href = "https://www.google.com";
+}
