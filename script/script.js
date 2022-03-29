b93a54ee4bfc86b8e9eb153e777292a32c013439
OMGSAMUELRBR <66468335+OMGSAMUELRBR@users.noreply.github.com>
2022-03-29 09:42:41 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 var mensaje = confirm("Esta web esta en desarrollo, ¿te gustaría entrar a mi web?");
 //Detectamos si el usuario acepto el mensaje
 if (mensaje) {
-    alert("Gracias por aceptar, te dirigeremos a mi web");
+alert("Gracias por aceptar, te dirigeremos a mi web");
 }
 //Detectamos si el usuario denegó el mensaje
 else {
 alert("Te dirigeremos a Google");
-    window.location.href = "https://www.google.com";
+    window.location.href = "http://www.google.com";
 }
